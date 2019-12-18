1250589485b36e04919f9856956e5dc5ccbb3216
Sorin Chis <kish.sorin@yahoo.com>
2019-12-18 13:36:26 +0200

    added costum font

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Welcome from "./components/WelcomeMessage";
 
 function App() {
   return (
     <div>
+      <Welcome />
       <p>Test</p>
     </div>
   );
---
+++ src/components/WelcomeMessage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function WelcomeMessage() {
+  return (
+    <div>
+      <h1>Welcome to CryptoDash</h1>
+    </div>
+  );
+}
