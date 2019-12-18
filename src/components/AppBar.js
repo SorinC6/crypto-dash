46c84be7cc3bc82a3d9e77b09a0931741b03becf
Sorin Chis <kish.sorin@yahoo.com>
2019-12-18 14:17:22 +0200

    added more style for app component

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,17 +1,43 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+const Logo = styled.div`
+  font-size: 1.5em;
+`;
 const Bar = styled.div`
   display: grid;
   grid-template-columns: 180px auto 100px 100px;
 `;
 
+const ControlButtonWrapper = styled.div`
+  cursor: pointer;
+  margin-bottom: 30px;
+  ${({ active }) =>
+    active &&
+    css`
+      text-shadow: 1px 1px 30px #03ff03;
+    `}
+`;
+
+const toUpperCase = string => {
+  return string.charAt(0).toUpperCase() + string.substr(1);
+};
+
+const ControlButton = ({ name, active }) => {
+  return (
+    <ControlButtonWrapper active={active}>
+      {toUpperCase(name)}
+    </ControlButtonWrapper>
+  );
+};
+
 export default () => {
   return (
     <Bar>
-      <div>CriptoDash</div>
+      <Logo>CriptoDash</Logo>
       <div />
-      <div>Dashboard</div>
-      <div>Settings</div>
+      <ControlButton name="dashboard" active />
+      <ControlButton name="settings" />
     </Bar>
   );
 };
