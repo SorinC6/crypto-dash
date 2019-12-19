200f4a5f2909ab2a94e9e73df943ba180a3fe00b
Sorin Chis <kish.sorin@yahoo.com>
2019-12-19 12:49:58 +0200

    create coin grid

--- src/components/Settings/CoinGrid.js
+++ src/components/Settings/CoinGrid.js
@@ -1,19 +1,21 @@
 import React, { useContext } from "react";
 import { AppContext } from "../../AppProvider";
 import styled from "styled-components";
+import { Tile, SelectableTile } from "../Shared/Tile";
 
 export const CoinGridStyled = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
+  gap: 15px;
 `;
 
 export default function CoinGrid() {
   const { state } = useContext(AppContext);
-  // console.log(state.coinList);
+  //console.log(state.coinList);
   return (
     <CoinGridStyled>
       {Object.keys(state.coinList).map(item => (
-        <div key={item}>{item}</div>
+        <SelectableTile key={item}>{item}</SelectableTile>
       ))}
     </CoinGridStyled>
   );
--- src/components/Settings/ConfirmButton.js
+++ src/components/Settings/ConfirmButton.js
@@ -1,11 +1,17 @@
 import React, { useContext } from "react";
 import styled from "styled-components";
 import { AppContext } from "../../AppProvider";
+import { fontSize1, greenBoxShadow, color3 } from "../Shared/Styles";
 
 const ConfirmBtnStyles = styled.div`
   margin: 20px;
-  color: green;
+  color: ${color3};
+  ${fontSize1}
+  padding:5px;
   cursor: pointer;
+  &:hover {
+    ${greenBoxShadow}
+  }
 `;
 
 const CenterDiv = styled.div`
