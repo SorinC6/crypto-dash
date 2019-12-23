421d6b30c49401b8af1b9dba5c672e660058923b
Sorin Chis <kish.sorin@yahoo.com>
2019-12-23 17:04:59 +0200

    added hightcharts costum theme

--- src/components/Dashboard/PriceChart.js
+++ src/components/Dashboard/PriceChart.js
@@ -2,8 +2,9 @@ import React, { useContext } from "react";
 import { AppContext } from "../../AppProvider";
 import config from "./HighchartsConfig";
 import { Tile } from "../Shared/Tile";
-// import ReactHicharts from "react-highcharts";
+import HighchartTheme from "./HighChartsTheme";
 const ReactHighcharts = require("react-highcharts"); // Expects that Highcharts was loaded in the code.
+ReactHighcharts.Highcharts.setOptions(HighchartTheme);
 
 export default function PriceChart() {
   const { state } = useContext(AppContext);
