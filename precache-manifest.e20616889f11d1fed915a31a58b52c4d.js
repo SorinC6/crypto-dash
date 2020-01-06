c7a7799f5b8fd56a8183ae954a6db5968690207a
Sorin Chis <kish.sorin@yahoo.com>
2020-01-06 11:25:00 +0200

    Updates

---
+++ precache-manifest.e20616889f11d1fed915a31a58b52c4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db6bceaa50bafbca6e1513377b5260d",
+    "url": "/crypto-dash/index.html"
+  },
+  {
+    "revision": "7ede343be44185664dba",
+    "url": "/crypto-dash/static/css/main.1f9172b1.chunk.css"
+  },
+  {
+    "revision": "d3066322abe4454fb43e",
+    "url": "/crypto-dash/static/js/2.436655bb.chunk.js"
+  },
+  {
+    "revision": "396656086d233a6f84f93b6161ecfda3",
+    "url": "/crypto-dash/static/js/2.436655bb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ede343be44185664dba",
+    "url": "/crypto-dash/static/js/main.c0c4bd00.chunk.js"
+  },
+  {
+    "revision": "4568b06c312b168da52d",
+    "url": "/crypto-dash/static/js/runtime-main.1d105a33.js"
+  }
+]);
